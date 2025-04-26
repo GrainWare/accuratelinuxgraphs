27119ffaa3acdf015aed9d456d221c3e6e76c1b4
electron271 <66094410+electron271@users.noreply.github.com>
2025-04-26 12:24:08 -0500

    Refactor code formatting and improve consistency across files
    - Adjusted indentation and formatting in `graph.astro`, `index.astro`, and `global.css` for better readability.
    - Cleaned up animation keyframes in `global.css` to maintain consistent formatting.
    - Updated descriptions and authors in `vars.ts` for clarity and consistency.
    - Ensured proper formatting in `tsconfig.json` and `wrangler.jsonc` for improved maintainability.

--- src/ts/vars.ts
+++ src/ts/vars.ts
@@ -1,9 +1,9 @@
 export const descriptions = [
-	"the most accurate linux graphs",
-	"your #1 source for accuate linux graphs",
-	"accuracy is love accuracy is life ACCURACY IS PRIORITY",
-	"the last source of accurate linux graphs",
-	`sponsored by
+  "the most accurate linux graphs",
+  "your #1 source for accuate linux graphs",
+  "accuracy is love accuracy is life ACCURACY IS PRIORITY",
+  "the last source of accurate linux graphs",
+  `sponsored by
 accuratelinuxgraphs.com
 store.accuratelinuxgraphs.com
 stats.atl.dev
@@ -13,14 +13,14 @@ ARG Solutions (Alpine Regional Group)
 ];
 
 export const authors = [
-	"持續改善",
-	"accuracy records",
-	"accuracy",
-	"big accuracy",
-	"grainware",
-	"kai, kaizer, kaisen, kai cenat, piezer, kai scent, kiezen-chan, kai-san, kai zenat, kai zen kernel, kai from lego ninjago, kai smell, kai cent, kaicaesar, kai centos, kai raising cane's, continual improvement, kalizer, kai zee er, kaizimpson, kaza (like casa 🏠), kaizateering, kaizenburg, rising, kitezen, byezen, siren kaizington",
-	"foolish idiots like you who bought stuff from store.accuratelinuxgraphs.com",
-	"$$$$ MONEY INCORPORATED $$$$",
-	"TUX",
-	"精確的 linux 圖表 中國資料收集部門",
+  "持續改善",
+  "accuracy records",
+  "accuracy",
+  "big accuracy",
+  "grainware",
+  "kai, kaizer, kaisen, kai cenat, piezer, kai scent, kiezen-chan, kai-san, kai zenat, kai zen kernel, kai from lego ninjago, kai smell, kai cent, kaicaesar, kai centos, kai raising cane's, continual improvement, kalizer, kai zee er, kaizimpson, kaza (like casa 🏠), kaizateering, kaizenburg, rising, kitezen, byezen, siren kaizington",
+  "foolish idiots like you who bought stuff from store.accuratelinuxgraphs.com",
+  "$$$$ MONEY INCORPORATED $$$$",
+  "TUX",
+  "精確的 linux 圖表 中國資料收集部門",
 ];
