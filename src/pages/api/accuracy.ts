0fd09b127c3f84789508aff16f68bf0ec6b50486
electron271 <66094410+electron271@users.noreply.github.com>
2025-05-01 21:22:06 -0500

    fix: increase generated lines length in accuracy endpoint

--- src/pages/api/accuracy.ts
+++ src/pages/api/accuracy.ts
@@ -15,7 +15,7 @@ export const GET: APIRoute = async () => {
   // Generate 1000 lines of babble, each max 1000 words
   const lines: string[] = [];
   for (let i = 0; i < 1000; i++) {
-    lines.push(markov.generate(10));
+    lines.push(markov.generate(500));
   }
 
   // Return as plain text
