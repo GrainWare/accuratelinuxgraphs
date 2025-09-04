268cdf91567dd8fca02629042d228ef25ebe6c05
electron271 <66094410+electron271@users.noreply.github.com>
2025-09-04 17:25:04 -0500

    remove chat

--- src/pages/api/chat.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-import ChatController from "../../controllers/chat";
-
-export const POST: APIRoute = async ({ request }) => {
-  const { message } = await request.json();
-  ChatController.getInstance().addMessage(message);
-  return new Response(null, { status: 204 });
-};
