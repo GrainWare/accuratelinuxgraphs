a8d21a0a897429e0aae21076fc2f952d867f01f3
electron271 <66094410+electron271@users.noreply.github.com>
2025-09-07 16:43:12 -0500

    improve backwards compatability

--- src/pages/files/img/[filename].ts
+++ src/pages/files/img/[filename].ts
@@ -1,9 +1,11 @@
 import "astro";
 import type { APIRoute } from "astro";
 import graphsData from "../../../data/graphs.json";
+import Fuse from "fuse.js";
 
 export const GET: APIRoute = ({ params }) => {
   const { filename } = params;
+
   if (!filename) {
     return new Response(
       JSON.stringify({ error: "missing filename parameter" }),
@@ -31,14 +33,42 @@ export const GET: APIRoute = ({ params }) => {
     });
   }
 
-  return new Response(
-    JSON.stringify({ error: `inaccurate graph filename '${filename}'` }),
-    {
-      status: 404,
+  const fileName = filename.replace(/\.[^/.]+$/, "");
+  const graphsNewKey = graphsData.map((item) => ({
+    ...item,
+    filename:
+      item.graphImgUrl
+        .split("/")
+        .pop()
+        ?.replace(/\.[^/.]+$/, "") || "",
+  }));
+
+  const fuse = new Fuse(graphsNewKey, {
+    keys: ["filename"],
+    includeScore: true,
+  });
+
+  const fuzzyResults = fuse.search(fileName);
+
+  if (fuzzyResults.length > 0) {
+    const matchedImage = fuzzyResults[0].item as {
+      graphImgUrl: string;
+      filename: string;
+    };
+    return new Response(null, {
+      status: 302,
       headers: {
-        "Content-Type": "application/json",
+        Location: matchedImage.graphImgUrl,
         "Access-Control-Allow-Origin": "*",
       },
+    });
+  }
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "/assets/img/404.png",
+      "Access-Control-Allow-Origin": "*",
     },
-  );
+  });
 };
