ae08301ffdbc0ccca17d82aed8422fe92841cd74
electron271 <66094410+electron271@users.noreply.github.com>
2026-07-06 21:37:47 -0500

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,19 +12,11 @@ export default defineConfig({
       "Access-Control-Allow-Origin": "*",
     },
   },
-  integrations: [react()],
-  adapter: cloudflare(),
   output: "server",
   vite: {
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias: import.meta.env.PROD
-        ? {
-            "react-dom/server": "react-dom/server.edge",
-          }
-        : undefined,
-    },
     plugins: [tailwindcss()],
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
